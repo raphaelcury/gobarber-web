d2243e182934f6c5ade2531f12afca0449c2023c
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-30 17:12:26 -0300

    Profile page and route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
+
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
@@ -12,6 +14,7 @@ export default function Routes() {
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
