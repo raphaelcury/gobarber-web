598b004a280afbcc0653900c48d2b730e4cd2c35
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-29 22:05:26 -0300

    Fixed .eslintrc: off option without ''.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
+    'prettier',
     'prettier/react'
   ],
   globals: {
@@ -14,7 +14,6 @@ module.exports = {
     SharedArrayBuffer: 'readonly',
     __DEV__: true
   },
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -35,11 +34,10 @@ module.exports = {
       'warn',
       { extensions: ['.jsx', '.js']},
     ],
-    'import/prefer-default-export': 'off',
-    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'import/prefer-default-export': 'off','no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
     'react/jsx-one-expression-per-line': 'off',
     'global-require': 'off',
-    'react-native/no-raw-text': off,
+    'react-native/no-raw-text': 'off',
     'no-param-reassign': 'off',
     'no-underscore-dangle': 'off',
     camelcase: 'off',
