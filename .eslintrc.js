e019b1aca692c4fcd5b212960777453527cbc08b
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-05-29 21:52:10 -0300

    More eslint plugins, configs and rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -11,17 +12,21 @@ module.exports = {
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: true
   },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 11,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: [
     'react',
+    'jsx-a11y',
+    'import',
+    'react-hooks',
     'prettier',
   ],
   rules: {
@@ -31,7 +36,15 @@ module.exports = {
       { extensions: ['.jsx', '.js']},
     ],
     'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': off,
     'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
     'no-console': ['error', {allow: [ 'tron']}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
