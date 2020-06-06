e22666b8ab8d3e2289197f88ab3f3da6dc8b3275
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-06-06 20:04:38 -0300

    Unform pkg used in sigin and signup.

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Logo" />
-      <form>
-        <input type="text" placeholder="Seu nome" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Seu nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Entrar</button>
         <Link to="/">Já tenho conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
