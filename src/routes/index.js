8623938f0d5e5979a598e8b23f6d771402382869
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-06-06 17:23:04 -0300

    Configured root-import.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ import { Switch } from 'react-router-dom';
 
 import Route from './Route';
 
-import SignUp from '../pages/SignUp';
-import SignIn from '../pages/SignIn';
+import SignUp from '~/pages/SignUp';
+import SignIn from '~/pages/SignIn';
 
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
