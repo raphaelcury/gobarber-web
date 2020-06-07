0f181635c44d92aad5502094d14b74c0faa8e618
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-06-07 11:17:53 -0300

    Integrated Redux, Saga and Reactotron.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = { teste: 'teste' };
+
+export default function auth(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
